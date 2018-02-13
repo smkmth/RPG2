684c0e3558d2cfa10706f28bfcea75409be39076
smkmth <dandavidnoon@gmail.com>
2018-02-13 15:34:11 +0000

    Cleaning
    tried to do an orbital platform, which proved more trouble then its worth. in future i will just use a shader to achieve this effect

---
+++ Assets/Scripts/OnThe_appObject/Player/PlayerRotation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+
+public class PlayerRotation : MonoBehaviour{
+	public Transform backLeft;
+	public Transform backRight;
+	public Transform frontLeft;
+	public Transform frontRight;
+	public RaycastHit lr;
+	public RaycastHit rr;
+	public RaycastHit lf;
+	public RaycastHit rf;
+	public Vector3 upDir;
+	void Update () {
+		Physics.Raycast (backLeft.position + Vector3.up, Vector3.down, out lr);
+		Physics.Raycast (backRight.position + Vector3.up, Vector3.down, out rr);
+		Physics.Raycast (frontLeft.position + Vector3.up, Vector3.down, out lf);
+		Physics.Raycast (frontRight.position + Vector3.up, Vector3.down, out rf);
+		upDir = (Vector3.Cross (rr.point - Vector3.up, lr.point - Vector3.up) +
+		Vector3.Cross (lr.point - Vector3.up, lf.point - Vector3.up) +
+		Vector3.Cross (lf.point - Vector3.up, rf.point - Vector3.up) +
+		Vector3.Cross (rf.point - Vector3.up, rr.point - Vector3.up)
+		).normalized;
+		Debug.DrawRay (rr.point, Vector3.up);
+		Debug.DrawRay (lr.point, Vector3.up);
+		Debug.DrawRay (lf.point, Vector3.up);
+		Debug.DrawRay (rf.point, Vector3.up);
+
+		transform.up = upDir;
+	}
+}
+
+
